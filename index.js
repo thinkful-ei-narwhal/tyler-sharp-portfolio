b7ac94313cba3dc49663b92e6eb80c418342162c
Tyler Sharp <johntylersharp@gmail.com>
2020-05-25 20:46:03 -0400

    I completed all revisions requested

--- index.js
+++ index.js
@@ -8,22 +8,21 @@ function generateLandingPage() {
                              </div>
                         </div>
                         <div class="body div collapsed" id="body-div"></div>`;
-  const getDiv = document.querySelector('#main-div');
+  const getDiv = document.querySelector("#main-div");
   getDiv.innerHTML += htmlString;
 }
 
 function generateBioDiv() {
-  return  `<div class="bio-div about-me">
+  return `<div class="bio-div about-me">
             <h2>Passionate, Experienced Professional</h2>
             <img src="img/tylerheadshot.png" class="headshot" align="middle" alt="headshot of Tyler Sharp, smiling in a sunny, scenic Atlanta view. Birds are chirping and the nearby Braves stadium can be heard as a distant, dull roar." />
             <p>I've been a big fan of tech all my life. I built my first computer at 11 and was Captain in my Robotics Team in high school.</p>
             <p>But I'm a man of many passions, and for the last 7 years I was a theme park Operations Manager for Merlin Entertainments, where I got to work with iconic brands such as LEGOLAND and Madame Tussauds.</p> 
             <p>I traveled the world and lived in Los Angeles, San Francisco, Orlando, and England. I currently am back in my hometown of Atlanta, GA. I still love to travel and have <i>many</i> more countries to check off the list!</p>
             <p>My workplace philosophy is to keep things fun and productive. I bring a personal approach to everything that I do.</p>
-            <p>I have published a <a href="http://www.triggerwarningshortfiction.com/the-box/" alt="link to horror short story">horror short story</a> and a <a href="https://www.amazon.com/Kingmaker-Gods-That-Gamble-vol-ebook/dp/B07XHTM7XB" alt="Amazon link to sci-fi novel">sci-fi novel</a> and am currently working on a fantasy title.</p>
+            <p>I have published a <a href="http://www.triggerwarningshortfiction.com/the-box/" alt="link to horror short story" target="_blank">horror short story</a> and a <a href="https://www.amazon.com/Kingmaker-Gods-That-Gamble-vol-ebook/dp/B07XHTM7XB" alt="Amazon link to sci-fi novel" target="_blank">sci-fi novel</a> and am currently working on a fantasy title.</p>
             <p>I've recently joined Thinkful's Coding Academy. I hope to apply my previous experience, personal style, and newfound skillset to a company that is making a difference in the world.</p>
           </div>`;
-
 }
 
 function generatePortfolioDiv() {
@@ -31,51 +30,51 @@ function generatePortfolioDiv() {
             <h2>Portfolio</h2>
             <p>Here are some of the projects I've created during my bootcamp.</p>
             <h3>Quiz App</h3>
-            <a href="https://thinkful-ei-narwhal.github.io/quiz-app-Max-Tyler/" alt="link to Quiz App"><img src="img/quiz.png" class="portfolio-pic" alt="screenshot of Quiz App" /></a>
+            <a href="https://thinkful-ei-narwhal.github.io/quiz-app-Max-Tyler/" alt="link to Quiz App" target="_blank"><img src="img/quiz.png" class="portfolio-pic" alt="screenshot of Quiz App" /></a>
             <p><i>HTML, CSS, jQuery</i></p>
-            <p>This is a quiz-app that covers knowledge of Brazilian Jiu-Jitsu. The user can submit answers and get a score at the end.</p>
+            <p>This is a quiz-app that covers knowledge of Brazilian Jiu-Jitsu. Ideal audience is a beginner or hobbyist of BJJ. It was built as a basic project to learn jQuery in my bootcamp course. The user can submit answers and get a score at the end.</p>
             </div>`;
 }
 
 function generateContactDiv() {
   return `<div class="contact-div about-me">
             <h2>Contact</h2>
-            <p><a href="https://github.com/Redact0r" alt="Github link"><b>Github</b></a></p>
-            <p><a href="https://www.linkedin.com/in/tyler-sharp-1346a1a7/" alt="Github link"><b>LinkedIn</b></a></p>
-            <p><a href="mailto:johntylersharp@gmail.com">Email</a></p>
+            <p><a href="https://github.com/Redact0r" alt="Github link" target="_blank"><b>Github</b></a></p>
+            <p><a href="https://www.linkedin.com/in/tyler-sharp-1346a1a7/" alt="Linkedin link" target="_blank"><b>LinkedIn</b></a></p>
+            <p><a href="mailto:johntylersharp@gmail.com" alt="Email link" target="_blank">Email</a></p>
             </div>`;
 }
 
 //Event Listeners
 //All events generate all three Divs so users can have seemless scrolling experience.
 function bioListener() {
-  const getDiv = document.querySelector('#body-div');
-  if (getDiv.classList.contains('collapsed')) {
-    getDiv.classList.remove('collapsed');
+  const getDiv = document.querySelector("#body-div");
+  if (getDiv.classList.contains("collapsed")) {
+    getDiv.classList.remove("collapsed");
   }
-  getDiv.innerHTML = '';
+  getDiv.innerHTML = "";
   getDiv.innerHTML += generateBioDiv();
   getDiv.innerHTML += generatePortfolioDiv();
   getDiv.innerHTML += generateContactDiv();
 }
 
 function portfolioListener() {
-  const getDiv = document.querySelector('#body-div');
-  if (getDiv.classList.contains('collapsed')) {
-    getDiv.classList.remove('collapsed');
+  const getDiv = document.querySelector("#body-div");
+  if (getDiv.classList.contains("collapsed")) {
+    getDiv.classList.remove("collapsed");
   }
-  getDiv.innerHTML = '';
+  getDiv.innerHTML = "";
   getDiv.innerHTML += generatePortfolioDiv();
   getDiv.innerHTML += generateBioDiv();
   getDiv.innerHTML += generateContactDiv();
 }
 
 function contactListener() {
-  const getDiv = document.querySelector('#body-div');
-  if (getDiv.classList.contains('collapsed')) {
-    getDiv.classList.remove('collapsed');
+  const getDiv = document.querySelector("#body-div");
+  if (getDiv.classList.contains("collapsed")) {
+    getDiv.classList.remove("collapsed");
   }
-  getDiv.innerHTML = '';
+  getDiv.innerHTML = "";
   getDiv.innerHTML += generateContactDiv();
   getDiv.innerHTML += generateBioDiv();
   getDiv.innerHTML += generatePortfolioDiv();
@@ -84,14 +83,14 @@ function contactListener() {
 function main() {
   generateLandingPage();
   //Event Listeners
-  document.addEventListener('click', function(event) {
-    if (event.target.matches('.bio-btn')) {
+  document.addEventListener("click", function (event) {
+    if (event.target.matches(".bio-btn")) {
       bioListener();
     }
-    if (event.target.matches('.portfolio-btn')) {
+    if (event.target.matches(".portfolio-btn")) {
       portfolioListener();
     }
-    if (event.target.matches('.contact-btn')) {
+    if (event.target.matches(".contact-btn")) {
       contactListener();
     }
   });
